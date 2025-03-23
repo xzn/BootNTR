181f81a4a525ee2ad9714796384c60814ecdc83a
JS Deck <jsdeckerido@gmail.com>
2025-03-22 23:26:54 -0300

    less recompile

--- source/config.h
+++ source/config.h
@@ -12,12 +12,10 @@
 
 #define MINOR_REVISION (25)
 
-#ifndef CURRENT_CONFIG_VERSION
-#include "../config_ver.h"
-#endif
-#ifndef CURRENT_CONFIG_VERSION
+#if 0
 #define CURRENT_CONFIG_VERSION  (SYSTEM_VERSION(1, 0, 13) | MINOR_REVISION)
 #endif
+extern u32 CURRENT_CONFIG_VERSION;
 
 #define SECONDS_IN_WEEK     604800
 #define SECONDS_IN_DAY      86400
---
+++ source/version.c
@@ -0,0 +1,3 @@
+#include "main.h"
+#include "../config_ver.h"
+u32 CURRENT_CONFIG_VERSION = CONFIG_VERSION;
